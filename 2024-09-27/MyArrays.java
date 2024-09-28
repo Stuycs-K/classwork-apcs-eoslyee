c5f9a242d785dedc69bcb9f9363ac1ac9e5ed002
Estella Yee <eyee60@stuy.edu>
2024-09-28 18:19:07 -0400

    concat function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,7 +2,8 @@ public class MyArrays{
   public static void main(String[] args){
 
     int[] ary = {1, 2, 3, 4, 5,};
-
+    
+    // Test arrayToString()
     System.out.println("arrayToString(" + ary + "): " + arrayToString(ary));
 
     // Test returnCopy()
@@ -13,16 +14,16 @@ public class MyArrays{
     System.out.println(aryContent.equals(arrayToString(aryCopy)) && (ary != aryCopy));
 
     // Test concatArray()
-    int[] ary1 = "Hi, this is APCS";
-    int[] ary2 = "with Mr. Konstantinovich";
+    int[] ary1 = {1, 2, 3, 4, 5,};
+    int[] ary2 = {6, 7, 8, 9, 10};
 
-    System.out.println(arrayToSring(concatArray(ary1, ary2)));
+    System.out.println(arrayToString(concatArray(ary1, ary2)));
   }
 
   public static String arrayToString(int[] nums){
     String ans = "[";
     for (int i = 0; i < nums.length; i ++){
-      ans += nums[i] + i;
+      ans += nums[i];
       if (i != nums.length-1){
         ans += ", ";
       }
@@ -38,16 +39,18 @@ public class MyArrays{
     return ans;
   }
 
-  public static int[] concatArrady(int[] ary1, int[] ary2){
+  public static int[] concatArray(int[] ary1, int[] ary2){
     int[] ans = new int[ary1.length + ary2.length];
     int i = 0; 
-    while (i < ary1.length){
-      ans[i] = ary1[i];
-      i++;
-    }
-    while (i < ary2.length){
-      ans[i+ary1.length-1] = ary2[i];
-      i++;
+    while (i < ans.length){
+      for (int i1 = 0; i1 < ary1.length; i1 ++){
+        ans[i] = ary1[i1];
+        i ++; 
+      }
+      for(int i2 = 0; i2 < ary2.length; i2 ++){
+        ans[i] = ary2[i2];
+        i ++;
+      }
     }
     return ans;
   }
