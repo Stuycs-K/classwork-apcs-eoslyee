4b4d9690c8f1cafbaab3fc64434c16f5ad1690fe
Estella <eyee60@stuy.edu>
2024-11-04 10:59:14 -0500

    Stop and tests for ReadFile, haven't started Triangle

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -11,12 +11,34 @@
         File file = new File("ReadFile.java");//1
         Scanner input = new Scanner(file);
         //CODE THAT SCANS THE FILE.
-        input.close();//releases the file from your program
+        // Stop and test opening ReadFile.java and printing every other line of text.
+        while(input.hasNextLine()){
+          input.nextLine();
+          if (input.hasNextLine()){
+            System.out.println(input.nextLine());
+          }
+        }
 
-      } catch (FileNotFoundException ex) {
+        // Now try changing the program to only print a line of text if it contains a '{' character.
+        while(input.hasNextLine()){
+          String Input = input.nextLine();
+          if (Input.indexOf("{") != -1){
+            System.out.println(Input);
+          }
+        }
+
+
+
+        input.close();//releases the file from your program
+      }
+
+      catch (FileNotFoundException ex) {
         //File not found what should you do?
         System.out.println("File not found");
         return; //you can return from a void function just don't put a value.
       }
+
+      System.out.println("check odd lines");
+
     }
   }
