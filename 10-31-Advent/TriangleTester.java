7faf8bd75605dc0a597731b6c7cb79a8ce027059
Estella Yee <eyee60@stuy.edu>
2024-11-06 00:55:05 -0500

    tried part B, but I genuinely couln't figure it out

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -8,8 +8,14 @@ public class TriangleTester{
     String filename = "inputTri.txt"; 
     
     System.out.println(countTrianglesA(filename));
+    System.out.println(countTrianglesB(filename)); 
     
  }
+
+    // Function to check for a valid triangle
+    public static boolean isTriangle(int a, int b, int c){
+        return (a+b > c) && (b+c > a) && (a+c > b);
+    }
 
     // Part A 
 
@@ -41,9 +47,41 @@ public class TriangleTester{
         return count; 
     }
 
-    // Function to check for a valid triangle
-    public static boolean isTriangle(int a, int b, int c){
-        return (a+b > c) && (b+c > a) && (a+c > b);
-    }
+    // Part B
+    public static int countTrianglesB(String filename){
+        int count = 0; 
+        try {
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            
+            // Sort into an array
+            int[][] vals = new int[3][]; 
+            
+            int i = 0; 
+            while(input.hasNextLine()){
+                for (int j = 0; j < 3; j ++){
+                    vals[j][i] = input.nextInt(); 
+                    vals[j][i+1] = input.nextInt(); 
+                    vals[j][i+2] = input.nextInt();   
+                }
+                i ++; 
+            }    
 
+            for (int k = 0; k < i;  k ++){
+                int a = vals[0][k];
+                int b = vals[1][k];
+                int c = vals[2][k]; 
+                if (isTriangle(a, b, c)){
+                    count ++; 
+                }
+            }
+            input.close();//releases the file from your program
+        }
+
+        catch (FileNotFoundException ex) {
+            //File not found what should you do?
+            System.out.println("File not found");
+        }
+        return count; 
+    }
 }
