3e6ad8efa251ee9083a1bb8e1d4ac75be41487b1
Estella <eyee60@stuy.edu>
2024-11-19 10:57:17 -0500

    created dayTwo files

--- 2024-11-18-advent2016/dayTwo.java
+++ 2024-11-18-advent2016/dayTwo.java
@@ -5,27 +5,39 @@ import java.util.Scanner;
 public class dayTwo {
   public static void main(String[] args){
 
+    String filename = "dayTwoInput.java";
+  }
+
+  // public static String[] parse(String filename){
+  //
+  // }
+
+
+  public static int solve{
     //2 Opening a file requires a try/catch
+    int x = 1, y = 1;
+    int[][] keypad = new int[][]{{"1", "4", "7"}, {"2", "5", "8"}, {"3", "6", "9"}};
+    String code = "";
     try {
-      File file = new File("dayOneInput.java");//1
+      File file = new File(filename);//1
       Scanner input = new Scanner(file);
-      //CODE THAT SCANS THE FILE.
+
+      while (input.hasNextLine()){
+        String +=
       }
 
+
+
       input.close();//releases the file from your program
-    }
+      }
+
 
     catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
-      return; //you can return from a void function just don't put a value.
     }
+    return code; //you can return from a void function just don't put a value.
 
 
-  }
-
-  public static int countBlocks (File filename){
-    int x; int y; int i = 0;
-    
   }
 }
