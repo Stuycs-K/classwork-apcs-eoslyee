774b6c67f8fdaf7b23d2246fd6f0d624b9a47c74
Estella Yee <eyee60@stuy.edu>
2024-11-22 23:02:43 -0500

    part1 day four

--- 2024-11-18-advent2016/dayFour.java
+++ 2024-11-18-advent2016/dayFour.java
@@ -1,17 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class dayFour {
   public static void main(String[] args){
 
     String filename = "inputFour.txt";
-    //System.out.println(p1(filename));
+    System.out.println(p1(filename));
   }
 
   public static int p1(String filename){
-    int[] roomAry = new int[26];
+    int[] alphabet = new int[26];
     int sum = 0; 
     try {
       File file = new File(filename);
@@ -19,14 +19,23 @@ public class dayFour {
 
       while (input.hasNextLine()){
         String room = input.nextLine();
-        //System.out.println(line);
-        for (int i = 0; i < room.length(); i ++){
+        System.out.println(room);
+        // For every letter in the code (before the []), add 1 to the alphabet. 
+        for (int i = 0; i < room.indexOf("["); i ++){
+            //System.out.println(room.charAt(i));
             int charIndex = room.charAt(i) - 'a'; 
-            if (charIndex > 0 && charIndex < 25){
-                roomAry[charIndex] ++; 
+            //System.out.println(charIndex);
+            if (charIndex >= 0 && charIndex < 26){
+                alphabet[charIndex] ++; 
             }
         }
-        
+        System.out.println(Arrays.toString(alphabet)); 
+
+        // Get the 5 greatest indeces 
+        String code = "[";
+        for (int counter = 5; counter > 0; counter --){
+            
+        }
       }
       input.close();//releases the file from your program
       }
@@ -36,7 +45,6 @@ public class dayFour {
       System.out.println("File not found");
     }
     return sum; //you can return from a void function just don't put a value.
-
 
   }
 }
