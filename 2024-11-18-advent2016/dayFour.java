90253ddaf323185b387c0daa1a57f45f86f74002
Estella Yee <eyee60@stuy.edu>
2024-11-23 19:11:49 -0500

    completed max and checkroom functions

--- 2024-11-18-advent2016/dayFour.java
+++ 2024-11-18-advent2016/dayFour.java
@@ -8,7 +8,51 @@ public class dayFour {
 
     String filename = "inputFour.txt";
     System.out.println(p1(filename));
+
+    int[] testMaxAry = new int[]{1, 2, 3, 2, 3, 5}; 
+    System.out.println(returnMax(testMaxAry)); 
+    System.out.println(returnMax(testMaxAry)); 
+
+    String testRoom = "aaaaa-bbb-z-y-x-123[abxyz]"; 
+    System.out.println(checkRoom(testRoom)); // Should return true 
   }
+  
+  // Return the character with the most occurances in the line 
+  public static char returnMax(int[] alphabet){
+    int max = 0; 
+    char maxChar = 'a'; 
+    for (int i = 0; i < alphabet.length; i ++){
+        if (alphabet[i] > max){
+            max = alphabet[i]; 
+            maxChar = (char)(i + 'a'); 
+        }
+    }
+    alphabet[maxChar-'a'] = 0; 
+    return maxChar; 
+  }
+
+  // Return true if the checkSum properly represents the line
+  public static boolean checkRoom(String room){
+    String checkSum = room.substring(room.indexOf("["), room.length()-1); 
+    int [] alphabet = new int[26];
+    String realRoom = ""; 
+    // For every letter in the code (before the []), add 1 to the alphabet. 
+    for (int i = 0; i < room.indexOf("["); i ++){
+        //System.out.println(room.charAt(i));
+        int charIndex = room.charAt(i) - 'a'; 
+        //System.out.println(charIndex);
+        if (charIndex >= 0 && charIndex < 26){
+            alphabet[charIndex] ++; 
+        }
+    }
+    System.out.println(Arrays.toString(alphabet));
+    for (int i = 0; i < 5; i ++){
+        realRoom += returnMax(alphabet); 
+    }
+    System.out.println(realRoom);
+    return (realRoom.equals(checkSum)); 
+  }
+
 
   public static int p1(String filename){
     int[] alphabet = new int[26];
@@ -19,23 +63,11 @@ public class dayFour {
 
       while (input.hasNextLine()){
         String room = input.nextLine();
+        String checkSum = room.substring(room.indexOf("["), room.length()-1); 
         System.out.println(room);
-        // For every letter in the code (before the []), add 1 to the alphabet. 
-        for (int i = 0; i < room.indexOf("["); i ++){
-            //System.out.println(room.charAt(i));
-            int charIndex = room.charAt(i) - 'a'; 
-            //System.out.println(charIndex);
-            if (charIndex >= 0 && charIndex < 26){
-                alphabet[charIndex] ++; 
-            }
-        }
-        System.out.println(Arrays.toString(alphabet)); 
-
-        // Get the 5 greatest indeces 
-        String code = "[";
-        for (int counter = 5; counter > 0; counter --){
-            
-        }
+        if (checkRoom(room)){
+            sum += Integer.parseInt(checkSum); 
+        } 
       }
       input.close();//releases the file from your program
       }
