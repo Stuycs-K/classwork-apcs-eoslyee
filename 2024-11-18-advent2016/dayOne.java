99a5fe0633bcb8dec9fff992c447c1fd4b1eab46
Estella Yee <eyee60@stuy.edu>
2024-11-25 20:08:56 -0500

    dayOne part 2

--- 2024-11-18-advent2016/dayOne.java
+++ 2024-11-18-advent2016/dayOne.java
@@ -1,11 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class dayOne {
   public static void main(String[] args){
     
-    String filename = "dayOneInput.txt"; 
+    String filename = "inputOne.txt"; 
 
     System.out.println(distance(filename)); 
   
@@ -14,6 +16,7 @@ public class dayOne {
   public static int distance(String filename){
     //2 Opening a file requires a try/catch
     int x = 0; int y = 0; int orientation = 0;
+    ArrayList<String> visitedCoors = new ArrayList<String>(); 
     try {
       File file = new File(filename); 
       Scanner input = new Scanner(file);
@@ -21,35 +24,29 @@ public class dayOne {
       String[] directions = data.split(", "); 
 
       for (int i = 0; i < directions.length; i ++){
-        if (directions[i].charAt(0) == 'R'){
-          if (orientation == 3){
-            orientation = 0; 
+        // Adjust orientation based on turn
+        if (directions[i].charAt(0) == 'R') {
+          orientation = (orientation + 1) % 4; // Right turn
+        } else {
+          orientation = (orientation + 3) % 4; // Left turn
+        }
+        int blocks = Integer.parseInt((directions[i].substring(1)));
+        int[][] increment = new int[][]{
+          {0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for (int j = 0; j < blocks; j ++){
+          x += increment[orientation][0]; 
+          y += increment[orientation][1];
+
+          String currentCoor = "(" + x + ", " + y + ")";
+          for (int k = 0; k < visitedCoors.size(); k ++){
+            if (currentCoor.equals(visitedCoors.get(k))){
+              input.close();
+              return Math.abs(x) + Math.abs(y);
+            }
           }
-          else {
-            orientation ++; 
-          }
-        }
-        else {
-          if (orientation == 0){
-            orientation = 3;
-          }
-          else {
-            orientation --; 
-          }
-        }
-        int blocks = Integer.parseInt((directions[i].substring(1))); 
-        if (orientation == 0){
-          y += blocks; 
-        }
-        if (orientation == 1){
-          x += blocks; 
-        }
-        if (orientation == 2){
-          y -= blocks; 
-        }
-        if (orientation == 3){
-          x -= blocks; 
-        }
+          visitedCoors.add(currentCoor); 
+        } 
+        //System.out.println(visitedCoors);
       }
       input.close();//releases the file from your program
     }
@@ -59,6 +56,6 @@ public class dayOne {
       System.out.println("File not found");
     }
 
-    return (Math.abs(x) + Math.abs(y));
+    return -1;
   }
 }
