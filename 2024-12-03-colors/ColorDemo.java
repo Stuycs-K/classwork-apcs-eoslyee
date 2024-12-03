3c941fa0f4917897fa2018d3a5d80bc2cc5e5aee
Estella <eyee60@stuy.edu>
2024-12-03 11:16:56 -0500

    more testing

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -14,4 +14,19 @@ public class ColorDemo{
     }
 
   }
+
+  public static int BLACK = 30;
+  public static int RED = 31;
+
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void color(int foreground, int background, int modifier){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+  }
+
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
 }
