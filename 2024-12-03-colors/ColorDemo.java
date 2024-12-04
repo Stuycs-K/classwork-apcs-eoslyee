054f9f9c594e34af43c49f3716fcd87ed99493ad
Estella Yee <eyee60@stuy.edu>
2024-12-03 23:55:06 -0500

    pink and red checkerboard

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -13,6 +13,31 @@ public class ColorDemo{
         System.out.println();
     }
 
+    System.out.print("\u001b[1;4;34;102m");
+    System.out.print("Surprise");
+    System.out.println("\u001b[0m");
+      try{
+              Thread.sleep(500);
+      }catch(Exception e){
+      }
+    int g = 15;
+    int r = 100;
+    for (int index = 4; index > 0; index--){
+        for (int i = 100; i > 0; i--){
+        for(int b = 0; b < 252; b+=38){
+            System.out.print("\u001b[48;2;"+r+";"+g+";"+b+";7m");
+            System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m ");
+        }
+        }
+        for (int i = 100; i > 0; i--){
+        for(int b = 252; b > 0; b-=38){
+            System.out.print("\u001b[48;2;"+r+";"+g+";"+b+";7m");
+            System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m ");
+        }
+        }
+    }
+
+    System.out.print("\u001b[0m");
   }
 
   public static int BLACK = 30;
@@ -29,4 +54,6 @@ public class ColorDemo{
   public static void go(int r,int c){
     System.out.print("\u001b[" + r + ";" + c + "f");
   }
+
 }
+
