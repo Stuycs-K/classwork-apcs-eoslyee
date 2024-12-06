8ff95aac298251979351cc9b10eada30485b1b42
Estella <eyee60@stuy.edu>
2024-12-06 10:26:19 -0500

    Hi, you are out today so I can't explain to you in person that I thought this was the homework for yesterday and I did the whole thing at home last night by accident. That's why there are no further commits from in class today

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,37 +1,37 @@
-import java.util.Random; 
+import java.util.Random;
 
 public class Warrior extends Adventurer{
     private int rage, maxRage;
 
     public Warrior(String name, int hp){
-        super(name, hp); 
-        this.rage = 0; 
-        this.maxRage = 10; 
+        super(name, hp);
+        this.rage = 0;
+        this.maxRage = 10;
     }
     public String getSpecialName(){
-        return "Rage"; 
+        return "Rage";
     }
 
     public int getSpecial(){
-        return rage; 
+        return rage;
     }
 
-    // Assuming n is not greater than maxRage 
+    // Assuming n is not greater than maxRage
     public void setSpecial(int n){
-        rage = n; 
+        rage = n;
     }
 
     public int getSpecialMax(){
-        return maxRage; 
+        return maxRage;
     }
 
     public String attack(Adventurer other){
         int damage = (int)Math.random()*4 + 2;
         other.applyDamage(damage);
         if (other.getHP()<0){
-            other.setHP(0); 
+            other.setHP(0);
         }
-        rage += 2; 
+        rage += 2;
         rage = Math.min(rage, maxRage); // Cap at maxRage
         return getName() + " inflicted " + damage + " damage on " + other.getName() + "!";
     }
@@ -40,8 +40,8 @@ public class Warrior extends Adventurer{
         if (other.getHP()==0){
             return other.getName() + " is dead.";
         }
-        int heal = (int)Math.random()*3 + 1; 
-        other.setHP(Math.min(other.getHP()+heal, other.getmaxHP())); 
+        int heal = (int)Math.random()*3 + 1;
+        other.setHP(Math.min(other.getHP()+heal, other.getmaxHP()));
         return other.getName() + " healed " + heal + " health!";
     }
 
@@ -51,20 +51,20 @@ public class Warrior extends Adventurer{
         }
         int heal = (int) Math.random()*3 + 1;
         setHP(Math.min(getHP()+heal, getmaxHP()));
-        return getName() + " healed " + heal + " health!";
+        return getName() + " healed " + heal + " hearts!";
     }
 
     public String specialAttack(Adventurer other){
         if (rage < 7){
             return getName() + " is not angry enough to perform special attack!";
         }
-        rage -= 7; 
-        int damage = (int)Math.random()*6+4; 
-        other.applyDamage(damage); 
+        rage -= 7;
+        int damage = (int)Math.random()*6+4;
+        other.applyDamage(damage);
         if (other.getHP()<0){
-            other.setHP(0); 
+            other.setHP(0);
         }
-        return getName() + " performed a special attack! " + other.getName() + " suffered " + damage + " damage."; 
+        return getName() + " performed a special attack! " + other.getName() + " suffered " + damage + " damage.";
     }
 
 }
