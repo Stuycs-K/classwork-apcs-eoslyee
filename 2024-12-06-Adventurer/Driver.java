d2b9cd57717cba0f6f152be6d6076ee9e05e27d9
Estella Yee <eyee60@stuy.edu>
2024-12-06 00:01:39 -0500

    adjusted cases when player is already dead, and implemented special attack

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -7,6 +7,13 @@ public class Driver{
         System.out.println(p1.attack(p2)); 
         System.out.println(p2 + "'s health = " + p2.getHP());
 
+        for (int i = 0; i <= 7; i ++){
+            p1.attack(p2); 
+        }
+        System.out.println(p1.specialAttack(p2)); 
+        System.out.println(p2 + "'s health = " + p2.getHP()); 
+        System.out.println(p1.specialAttack(p2)); // Should not special attack
+
         // Test support 
         System.out.println(p1.support(p2)); 
         System.out.println(p2 + "'s health = " + p2.getHP());
--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -28,25 +28,43 @@ public class Warrior extends Adventurer{
     public String attack(Adventurer other){
         int damage = (int)Math.random()*4 + 2;
         other.applyDamage(damage);
+        if (other.getHP()<0){
+            other.setHP(0); 
+        }
         rage += 2; 
         rage = Math.min(rage, maxRage); // Cap at maxRage
         return getName() + " inflicted " + damage + " damage on " + other.getName() + "!";
     }
 
     public String support(Adventurer other){
+        if (other.getHP()==0){
+            return other.getName() + " is dead.";
+        }
         int heal = (int)Math.random()*3 + 1; 
         other.setHP(Math.min(other.getHP()+heal, other.getmaxHP())); 
         return other.getName() + " healed " + heal + " health!";
     }
 
     public String support(){
+        if (getHP()==0){
+            return (getName() + " is dead.");
+        }
         int heal = (int) Math.random()*3 + 1;
         setHP(Math.min(getHP()+heal, getmaxHP()));
         return getName() + " healed " + heal + " health!";
     }
 
     public String specialAttack(Adventurer other){
-        return ""; 
+        if (rage < 7){
+            return getName() + " is not angry enough to perform special attack!";
+        }
+        rage -= 7; 
+        int damage = (int)Math.random()*6+4; 
+        other.applyDamage(damage); 
+        if (other.getHP()<0){
+            other.setHP(0); 
+        }
+        return getName() + " performed a special attack! " + other.getName() + " suffered " + damage + " damage."; 
     }
 
 }
