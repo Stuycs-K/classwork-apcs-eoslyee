85d234b8c06d4ba613d60059ed8abf52cc3fcbae
Estella Yee <eyee60@stuy.edu>
2024-12-05 23:50:15 -0500

    wrote attack and support methods

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,19 @@
 public class Driver{
     public static void main(String[] args){
         Adventurer p1 = new Warrior("Bob", 10); 
+        Adventurer p2 = new Warrior("Toby", 10); 
+
+        // Test attack 
+        System.out.println(p1.attack(p2)); 
+        System.out.println(p2 + "'s health = " + p2.getHP());
+
+        // Test support 
+        System.out.println(p1.support(p2)); 
+        System.out.println(p2 + "'s health = " + p2.getHP());
+
+        p1.setHP(3); 
+        System.out.println(p1.support()); 
+        System.out.println(p1 + "'s health = " + p1.getHP());
+
     }
 }
--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -5,33 +5,44 @@ public class Warrior extends Adventurer{
 
     public Warrior(String name, int hp){
         super(name, hp); 
+        this.rage = 0; 
+        this.maxRage = 10; 
     }
     public String getSpecialName(){
-        return ""; 
+        return "Rage"; 
     }
 
     public int getSpecial(){
-        return 0; 
+        return rage; 
     }
 
+    // Assuming n is not greater than maxRage 
     public void setSpecial(int n){
-
+        rage = n; 
     }
 
     public int getSpecialMax(){
-        return 0; 
+        return maxRage; 
     }
 
     public String attack(Adventurer other){
-        return "";
+        int damage = (int)Math.random()*4 + 2;
+        other.applyDamage(damage);
+        rage += 2; 
+        rage = Math.min(rage, maxRage); // Cap at maxRage
+        return getName() + " inflicted " + damage + " damage on " + other.getName() + "!";
     }
 
     public String support(Adventurer other){
-        return "";
+        int heal = (int)Math.random()*3 + 1; 
+        other.setHP(Math.min(other.getHP()+heal, other.getmaxHP())); 
+        return other.getName() + " healed " + heal + " health!";
     }
 
     public String support(){
-        return "";
+        int heal = (int) Math.random()*3 + 1;
+        setHP(Math.min(getHP()+heal, getmaxHP()));
+        return getName() + " healed " + heal + " health!";
     }
 
     public String specialAttack(Adventurer other){
