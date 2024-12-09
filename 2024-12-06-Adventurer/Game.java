9e04b55e83ad15220c6c71826fa471b2cf9dff93
Estella <eyee60@stuy.edu>
2024-12-09 11:00:25 -0500

    Set up player and enemy

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 public class Game {
   public static void main(String[] args) {
+    Adventurer player = new Warrior("Katniss");
+    Adventurer enemy = new CodeWarrior("Konstantinovich");
 
+    String playerHP =  player.getHP() + "/" + player.getmaxHP();
+    String playerSpecial = player.getSpecial() + "/" + player.getSpecialMax();
+    System.out.println("Your stats: " + " Health = " + playerHP + ", Special = " + playerSpecial);
+    String enemyHP =  enemy.getHP() + "/" + enemy.getmaxHP();
+    String enemySpecial = enemy.getSpecial() + "/" + enemy.getSpecialMax();
+    System.out.println("Enemy stats: Health =" + enemyHP +", Special = " + enemySpecial);
 
-    
     //do this once
     Scanner userInput = new Scanner(System.in);
 
     //You can do the rest many times:
 
     System.out.println("Enter username");
-    //Read one line of user input
+    //Read one line of user inputCreate a class Game.java in the same classwork directory.
     String userName = userInput.nextLine();
     //Do something with the input
     System.out.println("Username is: " + userName);
